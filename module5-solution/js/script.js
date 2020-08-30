25698a6ef82cb085d44d3c1d9987ba652b41f130
Tomas-Avizinis <tomas.avizinis@gmail.com>
2020-08-30 18:27:52 +0300

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowCategoriesHTML;
+  buildAndShowHomeHTML;
   },
   // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
@@ -124,6 +124,7 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
