a198e0db67b6401f4d5e1b0f2790386e897f07b0
Tomas-Avizinis <68870961+Tomas-Avizinis@users.noreply.github.com>
2021-07-01 20:50:00 +0300

    Add files via upload

---
+++ blogas/dom2-blogg.js
@@ -0,0 +1,78 @@
+
+function naujasIrasas (){
+    // const antraste = document.getElementById('txtAntraste').value;
+    const tekstas = document.getElementById('txtTekstas').value;
+
+    var visasBlogas = document.getElementById('blogoIrasai');
+    var naujaAntraste, naujasIrasas, atskyrimas;
+
+    //be tarpinio kintamojo
+    naujaAntraste = document.createElement ('p');
+    visasBlogas.appendChild(naujaAntraste);
+    naujaAntraste.textContent = document.getElementById('txtAntraste').value;
+
+    //su tarpiniu kintumuoju "tekstas" 
+    naujasIrasas = document.createElement('p');
+    visasBlogas.appendChild(naujasIrasas);
+    naujasIrasas.textContent = tekstas;
+
+    atskyrimas = document.createElement('hr');
+    visasBlogas.appendChild(atskyrimas);
+
+
+}
+
+function naujasIrasas2 (){
+    
+    var visasBlogas = document.getElementById('blogoIrasai');
+    var blogoIrasas, irasoData, irasoAntraste, irasoTekstas, atskyrimas;
+    var d = new Date(); 
+
+    // d = getDate();
+    atskyrimas = document.createElement('hr');
+    visasBlogas.firstElementChild.prepend(atskyrimas); //atskyrimas prieš pirmą elementą
+
+
+    blogoIrasas = document.createElement('div');
+    atskyrimas.before(blogoIrasas); //naujas įrašas prieš atskyrimą
+    blogoIrasas.className = 'irasas';
+
+    irasoData = document.createElement('p');
+    irasoData.className = 'dataLaikas';
+    irasoAntraste = document.createElement('h2');
+    irasoTekstas = document.createElement('p');
+    blogoIrasas.appendChild (irasoData);
+    blogoIrasas.appendChild(irasoAntraste);
+    blogoIrasas.appendChild(irasoTekstas);
+
+    // irasoAntraste.style--class
+
+    irasoData.textContent = d;
+    irasoAntraste.textContent = document.getElementById('txtAntraste').value;
+    irasoTekstas.textContent = document.getElementById('txtTekstas').value;
+
+    console.log(d);
+}
+
+function naujasIrasas3 () {
+    
+    let blogoIrasas;
+    let visasBlogas = document.getElementById('blogoIrasai');
+
+    isorinisIrasas.onload= function() {
+        if (isorinisIrasas.status===200) {
+            blogoIrasas = document.createElement('div');
+            blogoIrasas.innerHTML= isorinisIrasas.responseText;
+            visasBlogas.appendChild(blogoIrasas);
+        }
+    }
+    console.log (isorinisIrasas.responseText);
+}
+
+let isorinisIrasas= new XMLHttpRequest ();
+
+isorinisIrasas.open('GET', 'DOM2-blog-new1.html', true);
+isorinisIrasas.send();
+
+document.getElementById("mygtukasIrasas").addEventListener("click", naujasIrasas2);
+document.getElementById("mygtukasAdd").addEventListener("click", naujasIrasas3);
